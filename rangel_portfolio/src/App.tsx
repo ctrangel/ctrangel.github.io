e505325c7f8fa77fc75ae0a990ea19342adaa9f7
ctrangel <ctrangel@pointpark.edu>
2023-11-24 19:28:48 -0500

    ADD: nav bar component

--- rangel_portfolio/src/App.tsx
+++ rangel_portfolio/src/App.tsx
@@ -1,25 +1,77 @@
-import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup";
-import Button from "./components/Button";
-import { useState } from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-  let items = ['butter', 'milk', 'eggs', 'bread', 'cheese']
-
-  const [alertVisible, setAlertVisibility] = useState(false);
-
   return (
     <div>
-      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
-      <ListGroup
-        heading="Heading"
-        items={items}
-        onSelectItem={(item) => console.log(item)}
-      />
-      <Button color="primary" children="OI OI ERwin PP...P P" onClick={() => setAlertVisibility(true)}/>
+      <Navbar />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//################# just tutorial stuff #################
+// import Alert from "./components/Alert";
+// import ListGroup from "./components/ListGroup";
+// import Button from "./components/Button";
+// import { useState } from "react";
+
+// function App() {
+
+//   let items = ['butter', 'milk', 'eggs', 'bread', 'cheese']
+
+//   const [alertVisible, setAlertVisibility] = useState(false);
+
+//   return (
+//     <div>
+//       { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
+//       <ListGroup
+//         heading="Heading"
+//         items={items}
+//         onSelectItem={(item) => console.log(item)}
+//       />
+//       <Button color="primary" children="OI OI ERwin PP...P P" onClick={() => setAlertVisibility(true)}/>
+//     </div>
+//   );
+// }
+
+// export default App;
