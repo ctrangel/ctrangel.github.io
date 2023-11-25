9e71aec79a3fd72ae7c8cd93ccd063aaa8fcd642
ctrangel <ctrangel@pointpark.edu>
2023-11-24 20:21:50 -0500

    ADD: tailwind

--- rangel_portfolio/src/components/Navbar.tsx
+++ rangel_portfolio/src/components/Navbar.tsx
@@ -1,13 +1,41 @@
-
-
-
+import React, { useState } from "react";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1>Navbar</h1>
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Rangel Portfolio
+        </a>
+        <button className="navbar-toggler" type="button" onClick={toggleNavbar}>
+          <span className="navbar-toggler-icon border-gray-950"></span>
+        </button>
+        <div
+          className={`collapse navbar-collapse ${
+            isNavbarOpen ? "show" : "hide"
+          }`}
+        >
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">Contact</a>
+            </li>
+          </ul>
         </div>
-    )
-}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
