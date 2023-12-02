53670264e623c5bedb486974013bd941be1ef5ce
ctrangel <ctrangel@pointpark.edu>
2023-12-02 00:45:28 -0500

    I'm tired

--- rangel_portfolio/src/App.tsx
+++ rangel_portfolio/src/App.tsx
@@ -1,41 +1,17 @@
+
+import AboutMe from "./components/AboutMe";
 import Navbar from "./components/Navbar";
 
 function App() {
-
-
-
   return (
     <div>
       <Navbar />
+      <AboutMe />
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
---
+++ rangel_portfolio/src/components/AboutMe.tsx
@@ -0,0 +1,43 @@
+const AboutMe = () => {
+  return (
+    <div className="about-me">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="text-center">About Me</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              voluptates. Quam voluptates, quod, quae, quia voluptatum quibusdam
+              dolorum fugiat quos exercitationem quas consequuntur? Quisquam,
+              voluptates. Quam voluptates, quod, quae, quia voluptatum quibusdam
+              dolorum fugiat quos exercitationem quas consequuntur? Quisquam,
+              voluptates. Quam voluptates, quod, quae, quia voluptatum quibusdam
+              dolorum fugiat quos exercitationem quas consequuntur? Quisquam,
+              voluptates. Quam voluptates, quod, quae, quia voluptatum quibusdam
+              dolorum fugiat quos exercitationem quas consequuntur? Quisquam,
+              voluptates. Quam voluptates, quod, quae, quia voluptatum quibusdam
+              dolorum fugiat quos exercitationem quas consequuntur? Quisquam,
+              voluptates. Quam voluptates, quod, quae, quia voluptatum quibusdam
+              dolorum fugiat quos exercitationem quas consequuntur? Quisquam,
+              voluptates. Quam voluptates, quod, quae, quia voluptatum quibusdam
+              dolorum fugiat quos exercitationem quas consequuntur? Quisquam,
+              voluptates. Quam voluptates, quod, quae, quia voluptatum quibusdam
+              dolorum fugiat quos exercitationem quas consequuntur? Quisquam,
+              voluptates. Quam voluptates, quod, quae, quia voluptatum quibusdam
+              dolorum fugiat quos exercitationem quas consequuntur? Quisquam,
+              voluptates. Quam voluptates, quod, quae, quia voluptatum quibusdam
+              dolorum fugiat quos exercitationem quas consequuntur? Quisquam,
+              volupt
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
