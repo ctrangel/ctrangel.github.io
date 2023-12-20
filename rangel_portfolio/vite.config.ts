4e4a94162dce66ad810fcb67fd5515af229f8de8
ctrangel <ctrangel@pointpark.edu>
2023-12-19 22:23:56 -0500

    Update vite.config.ts

--- rangel_portfolio/vite.config.ts
+++ rangel_portfolio/vite.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     watch: {
+//       usePolling: true,
+//     },
+//   },
+// })
+
 export default defineConfig({
+  base: '/ctrangel.github.io/',
   plugins: [react()],
   server: {
     watch: {
@@ -10,4 +20,3 @@ export default defineConfig({
     },
   },
 })
-
